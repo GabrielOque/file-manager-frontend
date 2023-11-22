cdcc71c9da5d41c2ea4ea7b29c5201f1d83ff804
Gabriel Oquendo <oquendogabriel18@gamil.com>
2023-11-22 15:38:51 -0500

    generate an context provider

---
+++ src/context/ContextProvider.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect, useContext, createContext } from "react";
+import {
+  requestLogin,
+  updateRequest,
+  requestUsersFaculties,
+  requestFaculties,
+  requestFiles,
+  requestLogout,
+} from "../api/usersRequest";
+const context = createContext();
+
+export const useContextProvider = () => {
+  return useContext(context);
+};
+
+export const ContextProvider = ({ children }) => {
+  const [authenticated, setAuthenticated] = useState();
+  const [userFaculties, setUserFaculties] = useState([]);
+  const [faculties, setFaculties] = useState([]);
+  const [files, setFiles] = useState([]);
+
+  const userLogin = async (data) => {
+    const response = await requestLogin(data);
+    setAuthenticated(response.data);
+  };
+
+  const updateUser = async (data) => {
+    const response = await updateRequest(data);
+    setAuthenticated(response.data);
+  };
+
+  const getUsersFaculties = async (id) => {
+    const response = await requestUsersFaculties(id);
+    console.log(response.data);
+    setUserFaculties(response.data);
+  };
+
+  const getFaculties = async () => {
+    const response = await requestFaculties();
+    setFaculties(response.data);
+  };
+
+  const getFiles = async (idAuthor) => {
+    const response = await requestFiles(idAuthor);
+    console.log(response.data);
+    setFiles(response.data);
+  };
+
+  const logout = async () => {
+    await requestLogout();
+    setAuthenticated("");
+  };
+
+  return (
+    <context.Provider
+      value={{
+        authenticated,
+        setAuthenticated,
+        userLogin,
+        updateUser,
+        userFaculties,
+        getUsersFaculties,
+        faculties,
+        getFaculties,
+        files,
+        getFiles,
+        logout,
+      }}
+    >
+      {children}
+    </context.Provider>
+  );
+};
