9df1e3d66224a8f75349b857b650d69cfb4efabc
Gabriel Oquendo <oquendogabriel18@gamil.com>
2023-11-22 15:41:39 -0500

    config constants

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,19 @@
 export const VITE_API_URL = import.meta.env.VITE_API_URL;
+export const ROLES = {
+  ADMIN: "Admin",
+  SUPER_ADMIN: "SuperAdmin",
+  USER: "User",
+};
+
+export const CREDENTIALS = {
+  withCredentials: true,
+};
+
+export const MESSAGE = {
+  SUCCESS: "success",
+  ERROR: "error",
+  WARNING: "warning",
+  INFO: "info",
+  DEFAULT: "default",
+  LOADING: "loading",
+};
