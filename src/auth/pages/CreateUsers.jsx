3faa9a4893df48d4c831f6b633e792cdf37c3847
Gabriel Oquendo <oquendogabriel18@gamil.com>
2023-11-22 15:42:01 -0500

    page form create new user

---
+++ src/auth/pages/CreateUsers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateUsers = () => {
+  return (
+    <div className="w-full bg-slate-500 h-screen flex justify-center items-center">
+      <h1 className="text-5xl  text-white">Crear usuarios</h1>
+    </div>
+  );
+};
+
+export default CreateUsers;
