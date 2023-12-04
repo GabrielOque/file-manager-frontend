2afd202477eb82f536b404350e8af740d08c5f0c
Gabriel Oquendo <oquendogabriel18@gamil.com>
2023-12-04 08:25:06 -0500

    confirm login

--- src/public/pages/Login.jsx
+++ src/public/pages/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
       } catch (error) {
         console.error("Error al verificar el token:", error);
         // Manejar el error y redirigir al usuario al login
-        navigate("/login");
+        navigate("/");
       }
     })();
   }, []);
